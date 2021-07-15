4376412be97efd76b8e5c4ee70b098c44cdb96f3
Resul Silay <resulsilay@gmail.com>
2021-07-15 22:15:54 +0300

    Initial Commit

---
+++ Shared/wicoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  wicoinApp.swift
+//  Shared
+//
+//  Created by Resul Silay on 15.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct wicoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
